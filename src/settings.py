e00d9dea3f6ca86704920404969deb561038e063
peixed351 <peixed351@gmail.com>
2024-03-06 21:33:04 +0300

    some check

--- src/settings.py
+++ src/settings.py
@@ -38,7 +38,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://epic-kitchens.github.io/VISOR/"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 14982214
+PREVIEW_IMAGE_ID: int = 15001759
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/epic-kitchens-visor"
