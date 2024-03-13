fd3f671bf6c88503c01c04cb7a927d8783eaa649
peixed351 <peixed351@gmail.com>
2024-03-13 14:52:00 +0300

    some check

--- src/settings.py
+++ src/settings.py
@@ -28,7 +28,7 @@ CV_TASKS: List[CVTask] = [
     CVTask.SemanticSegmentation(),
     CVTask.ObjectDetection(),
     CVTask.Identification(),
-    CVTask.SemiSupervised(),
+    # CVTask.SemiSupervised(),
 ]
 ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.SemanticSegmentation()]
 
@@ -39,7 +39,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://epic-kitchens.github.io/VISOR/"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 15001759
+PREVIEW_IMAGE_ID: int = 15429464
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/epic-kitchens-visor"
